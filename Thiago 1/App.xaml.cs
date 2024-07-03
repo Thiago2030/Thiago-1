1da8663a0d992e3d22bbe9c4a203dde6a1e8ad7a
Thiago <Thiago@DESKTOP-HLR6O3L>
2024-07-03 14:03:30 -0300

    Adicionar arquivos de projeto.

---
+++ Thiago 1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Thiago_1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Thiago 1/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Thiago_1
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
